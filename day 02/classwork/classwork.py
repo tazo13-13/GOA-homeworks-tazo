7bdae00ea23ff3f96fb9d45fc01d09c8436d51d7
Lasha-30 <lashatertera30@gmail.com>
2025-10-13 16:22:31 +0400

    second commit

---
+++ day 02/classwork/classwork.py
@@ -0,0 +1,35 @@
+#ეს არის მონაცემების რამდენიმე სხვადასხვა სახეობა რომელიც გამოიყენება ინფორმაციის ტიპის გასარჩევად ცვლადში
+
+# 1.integer(მთელი რიცხვი)
+age = 15
+year = 2025
+print(age, year)
+
+# 2.float(ათწილადი)
+height = 1.75
+print(height)
+
+# 3.string(ტექსტი)
+name = "tazo"
+school = "10th grade"
+print(name, school)
+
+# 4.boolean(ლოგიკური მნიშვნელობა)
+is_student = True
+is_teacher = False
+print(is_student, is_teacher)
+
+# 5.list(სია)
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+
+# 6.tuple(უცვლელი სია)
+cordinates = (100, 100)
+print(cordinates)
+
+#დავალება 2
+name = "luka"
+name = "tazo"
+print("me mqvia",name)
+
+#კომპიუტერი მხოლოდ ნულებს და ერთებს კითხულობს
