1709f35dd227ab42576487fc0d9ca3f50c0abb65
Lasha-30 <lashatertera30@gmail.com>
2026-03-07 17:37:28 +0400

    commit

---
+++ day 21/classwork/index.py
@@ -0,0 +1,34 @@
+# 1) append()
+fruit = ["apple", "orange"]
+fruit.append("banana")
+print(fruit)
+
+
+# 2) lower()
+name = "Tazo"
+name1 = name.lower()
+print(name1)
+
+
+# 3) upper()
+last_name = "terterashvili"
+last_name1 = last_name.upper()
+print(last_name1)
+
+
+# 4) capitalize()
+nikname = "akeb"
+real_nikname = nikname.capitalize()
+print(real_nikname)
+
+
+# 5) pop()
+friends = ["lasha", "lukaku", "gio", "levani", "luka"]
+imposter = friends.pop(1)
+print(friends)
+
+
+# 6) join()
+fruit = ["a" "p" "p" "l" "e"]
+apple1 = " ".join(fruit)
+print(apple1)
