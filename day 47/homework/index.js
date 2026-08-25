8937c867548cd1293838f452401aa81e65ebebe4
Lasha-30 <lashatertera30@gmail.com>
2026-08-25 17:22:08 +0400

    commit

---
+++ day 47/homework/index.js
@@ -0,0 +1,46 @@
+// 1)
+let money = 100
+
+money += 50
+money -= 30
+money *= 2
+money /= 2
+money++
+money--
+console.log(money)
+
+
+// 2)
+let score = 10
+
+score += 15
+score -= 5
+console.log(score)
+
+
+// 3)
+let number = 20
+
+number += 15
+console.log(number)
+
+number -=5
+console.log(number)
+
+number *=2
+console.log(number)
+
+number /= 5
+console.log(number)
+
+
+// 4)
+let myAge = 18
+myAge++
+console.log(myAge)
+
+
+// 5)
+let myFriendsAge = 23
+myFriendsAge--
+console.log(myFriendsAge)
